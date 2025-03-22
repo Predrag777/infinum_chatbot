51f3c66828609ed15efd407a58c15ba1520c75df
Predrag777 <predragbrajovicSS@gmail.com>
2025-03-22 07:30:08 +0100

    History bar created

--- frontend.py
+++ frontend.py
@@ -2,9 +2,27 @@ import streamlit as st
 
 st.title("Infinum JurisMind")
 
+# Save discussion history
+if 'history' not in st.session_state:
+    st.session_state.history = []   # Initialize history in session
+
+
+
+############################Main part
 # Place for the user prompt
 user_prompt=st.text_input("You: ")
 
 
+
+
+############################Sidebar
 # Sidebar where we would save history of conversation
 st.sidebar.title("History")
+
+
+# Check if there are old chats
+if st.session_state.history:
+    for i in st.session_state.history:
+        chat=f'<a href="#">Chat</a>'
+else:
+    st.sidebar.write("Empty history")
