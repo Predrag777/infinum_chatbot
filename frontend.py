acdadde7c539168d6cf7d4ee2d654e3b415f6d43
Predrag777 <predragbrajovicSS@gmail.com>
2025-03-22 08:29:01 +0100

    Simulate chating with bot

--- frontend.py
+++ frontend.py
@@ -9,7 +9,7 @@ try:
     response = requests.get('http://127.0.0.1:8000/history') # History API endpoint
     if response.status_code == 200:
         api_history = response.json()  
-        st.session_state.history = api_history  
+        st.session_state.global_history = api_history  
     else:
         st.error("Can not connect to server")
 except Exception as e:
@@ -18,10 +18,11 @@ except Exception as e:
 
 
 # Save discussion history
+if 'global_history' not in st.session_state:
+    st.session_state.global_history = []   # Initialize global history in session
+
 if 'history' not in st.session_state:
-    st.session_state.history = []   # Initialize history in session
-
-
+    st.session_state.history=[]
 
 ############################Main part
 # Place for the user prompt
@@ -39,6 +40,10 @@ if user_prompt:
         elif 'JurisMind' in msg:
             st.markdown(f"<p style='color: green'>{msg}</p>", unsafe_allow_html=True)
 
+if st.button('End chat'):
+    if st.session_state.history:
+        st.session_state.global_history.append(st.session_state.history)
+        st.session_state.history = []  # Reset chat
 
 
 ############################Sidebar
@@ -48,8 +53,8 @@ st.sidebar.title("History")
 
 
 # Check if there are old chats
-if st.session_state.history:
-    for idx, chat in enumerate(st.session_state.history):
+if st.session_state.global_history:
+    for idx, chat in enumerate(st.session_state.global_history):
         chat_text = f"Chat {idx + 1}: {chat[1]} -> {chat[2]}" # Answer from API
         st.sidebar.markdown(f'<a href="javascript:void(0);" onclick="window.parent.location.hash=\'chat{idx}\'">{chat_text}</a>', unsafe_allow_html=True)
 else:
